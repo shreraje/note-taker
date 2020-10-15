b48c3e2e08d4149067b7cafbbcb52d6b982302f3
Rajendra Shrestha <rajendrashr@gmail.com>
2020-10-14 22:10:41 -0700

    Setting up express app, initial port,referencing public folder, api routes & listener to start server

---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+// Series of npm packages that we will use to give our server useful functionality
+var express = require("express");
+
+// Sets up the Express App & initial port which will be used later in our listener
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Referencing public folder
+app.use(express.static('public'));
+
+// ================================================================================
+// ROUTER
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
