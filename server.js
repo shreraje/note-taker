879b3c0868045ca87ea2ad0766721babea149b4d
Rajendra Shrestha <rajendrashr@gmail.com>
2020-10-14 22:50:06 -0700

    Adding codes for htmlRoutes such as file path, html get request for notes.html & index.html

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static('public'));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
+// require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // =============================================================================
